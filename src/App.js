b89e8578743bf50bce5f69c3221410b6b2402cef
roxanastoian <131897578+roxanastoian@users.noreply.github.com>
2023-12-06 21:46:13 +0000

    Added JSX and CSS for the main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
-  return <div className="App">Hello</div>;
+export default function App() {
+  <Weather />;
 }
-
-export default App;
